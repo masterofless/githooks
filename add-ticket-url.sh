d2af5e26a1fd871f620d4adf2982b5585fa592c0
Andy Cohan <acohan@nexient.com>
2022-09-08 16:54:46 -0400

    epcml-1533 init add-ticket-url.sh and .pre-commit-hooks.yaml

---
+++ add-ticket-url.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+COMMIT_MSG_FILE=$1
+COMMIT_SOURCE=$2
+SHA1=$3
+
+head -1 "$COMMIT_MSG_FILE" | /usr/bin/perl -pe 's/.*epcml-(\d+).*/\n\nhttps:\/\/jira.oreillyauto.com\/browse\/EPCML-$1/i' >> "$COMMIT_MSG_FILE"
